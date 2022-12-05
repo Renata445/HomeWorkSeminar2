f503cde835cb8f5de7280312846a6fee25fd5f56
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-05 20:28:03 +0300

    Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите 5-ти значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 1999 || n > 100000 )
+{
+    if(n < 1999)
+    {
+    Console.WriteLine("Вы ошиблись! Введите 5-ти значное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    }
+    if(n > 100000)
+    {
+    Console.WriteLine("Вы ошиблись! Введите 5-ти значное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int e, d, s, t, dt;
+e = n % 10;
+d = (n % 100) / 10;
+s = (n % 1000) / 100;
+t = (n % 10000) / 1000;
+dt = n / 10000;
+if(dt == e && t == d)
+{
+    Console.WriteLine("Является палиндромом!");
+}
+else
+{
+    Console.WriteLine("Не является палинлиндромом!");
+}
+
